e297f3ca89948a307a872c354a6a2a0257772ce2
smizuka <satomizu2@hotmail.com>
2019-03-26 21:11:20 +0900

    morimoto-change

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,9 +7,6 @@ class SearchesController < ApplicationController
 
       #検索に該当するコードを取得する
       # @stocks=@stock.select("code,name").distinct.order("code")
-      @stock2=@stock.select("code,name")
-      @stock3=@stock2.distinct
-      @stocks=@stock3.order("code")
 
       # where(name like '%72%' or code like '%72%'))
     end
@@ -18,27 +15,14 @@ class SearchesController < ApplicationController
 
     # @stock.where(name like '%#%' or code like '%#{code}%')
     # SELECT DISTINCT code,name FROM "stocks" WHERE (name like '%72%' or code like '%72%') ORDER BY code)
-    p "--------------------------"
 
-    p @stock2
-    p "--------------------------"
-    p @stock3
     #レコードを配列にしていく
-    n= @stocks.count
-    p "--------------------------"
+    # n= @stocks.count
 
-    if n!=0
-      codes=[]
-      names=[]
-
-      @stocks.each do |stock|
-        codes.push(stock.code)
-        names.push(stock.name)
-      end
-
+    if @stock.count!=0
       res ={
-          code: codes,
-          name: names
+          code: @stock.map(&:code).uniq,
+          name: @stock.map(&:name).uniq
       }
     else
     # 該当する銘柄がない場合
@@ -52,6 +36,31 @@ class SearchesController < ApplicationController
       }
 
     end
+    # if n!=0
+    #   codes=[]
+    #   names=[]
+
+    #   @stocks.each do |stock|
+    #     codes.push(stock.code)
+    #     names.push(stock.name)
+    #   end
+
+    #   res ={
+    #       code: codes,
+    #       name: names
+    #   }
+    # else
+    # # 該当する銘柄がない場合
+
+    #   codes=[]
+    #   names=[]
+
+    #   res ={
+    #       code: codes,
+    #       name: names
+    #   }
+
+    # end
 
     render json: res.to_json
   end
