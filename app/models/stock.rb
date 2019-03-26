ed9476f078e2303a3871f03c28eb85e0c4077477
smizuka <satomizu2@hotmail.com>
2019-03-26 22:21:18 +0900

    test

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -8,9 +8,7 @@ class Stock < ApplicationRecord
   scope :search_by_keyword, -> (keyword) {
     # where("name like ? or code like ?","%#{keyword}%","%#{keyword}%") if keyword.present?
     # where("name like %#{keyword}%? or code like %#{keyword}%?") if keyword.present?
-    # where('name like "%#{keyword}%" or code like "%#{keyword}%"') if keyword.present?
-
-    where("code LIKE ?", "%#{keyword}%") if keyword.present?
+    where("name" like %#{keyword}% or "code" like %#{keyword}%) if keyword.present?
 
   }
 
